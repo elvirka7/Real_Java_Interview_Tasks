658b5fbb81227a9d547099eb6adae390e99d8bd3
elvirka7 <elvirka7@gmail.com>
2022-03-29 15:44:25 -0400

    Cognizant: Find the longest common substring among two strings

--- src/Cognizant/Map_LongestSubstring.java
+++ src/Cognizant/Map_LongestSubstring.java
@@ -1,41 +1,40 @@
 package Cognizant;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /*
 Find the longest common substring among two strings
-SOLUTION BY MANAS
  */
 public class Map_LongestSubstring {
-        public static void main(String[] args) {
-            String s1 =   "aaabbbccccuytutyutututycddddeeeeee";
-            String s2 =   "zzzzzccczzzzccddddeeeerrreeee";
-            String[] ar1 = s1.split("");
-            String[] ar2 = s2.split("");
-            Map<String, Integer> map1 = new HashMap<>();
-            Map<String, Integer> map2 = new HashMap<>();
-            for (String each : ar1) map1.put(each, Collections.frequency(Arrays.asList(ar1), each));
-            for (String each : ar2) map2.put(each, Collections.frequency(Arrays.asList(ar2), each));
 
-            int max=0;
-            String maxChar= "";
-            for (Map.Entry<String, Integer> each1 : map1.entrySet()) {
-                for (Map.Entry<String, Integer> each2 : map2.entrySet()) {
-                    if (each1.getKey().equals(each2.getKey()) && each1.getValue().equals(each2.getValue())){
-                        if ( each1.getValue()>max) {
-                            max = each1.getValue();
-                            maxChar = each1.getKey();
+    public static void main(String[] args) {
+
+    }
+            public static Set<String> longestCommonSubstrings(String s, String t) {
+                int[][] table = new int[s.length()][t.length()];
+                int longest = 0;
+
+                Set<String> result = new HashSet<>();
+
+                for (int i = 0; i < s.length(); i++) {
+                    for (int j = 0; j < t.length(); j++) {
+                        if (s.charAt(i) != t.charAt(j)) {
+                            continue;
+                        }
+
+                        table[i][j] = (i == 0 || j == 0) ? 1
+                                : 1 + table[i - 1][j - 1];
+                        if (table[i][j] > longest) {
+                            longest = table[i][j];
+                            result.clear();
+                        }
+                        if (table[i][j] == longest) {
+                            result.add(s.substring(i - longest + 1, i + 1));
                         }
                     }
                 }
+                return result;
             }
-            String result="";
-            for (int i = 0; i < max; i++) {
-                result +=maxChar;
-            }
-            System.out.println(result);
+
+
         }
-    }
